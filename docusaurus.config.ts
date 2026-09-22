7ec846afba0af13775d04b3eefa4025a77832f83
Ralf Hamacher <ralf.hamacher@googlemail.com>
2026-09-23 00:34:40 +0200

    footer config and index.md in docs/proj

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,6 +104,10 @@ const config: Config = {
               label: 'Tutorial',
               to: '/docs/guides/intro',
             },
+            {
+              label: 'Projects',
+              to: '/docs/projects',
+            },
           ],
         },
         {
